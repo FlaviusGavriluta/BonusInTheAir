abdddd5c7193e1b1dce310f3a03595fecaf66434
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-17 23:07:33 +0300

    remove .idea

--- src/main/java/com/codecool/bonusintheair/logic/CompanyProfitCalculator.java
+++ src/main/java/com/codecool/bonusintheair/logic/CompanyProfitCalculator.java
@@ -1,6 +1,5 @@
 package com.codecool.bonusintheair.logic;
 
-
 import com.codecool.bonusintheair.data.BonusRule;
 import com.codecool.bonusintheair.data.Broker;
 import com.codecool.bonusintheair.data.CompanyProfit;
@@ -15,28 +14,28 @@ public class CompanyProfitCalculator {
     }
 
     public CompanyProfit calculate(List<Broker> brs) {
-        int j=0;
-        int i=brs.size();
-        double t=0;
-        double s= 0;
-        double r =0;
-        while(i>0){
-            Broker b =brs.get(brs.size()-i);
-            t+= b.profit();
-            double s2=0;
-            j=bis.size();
-            while(j>0){
-                BonusRule bo= bis.get(bis.size()-j);
-                if(b.profit() >=bo.minimum()){
-                    s2= bo.multiplier();
-                    j =1;
+        int j = 0;
+        int i = brs.size();
+        double t = 0;
+        double s = 0;
+        double r = 0;
+        while (i > 0) {
+            Broker b = brs.get(brs.size() - i);
+            t += b.profit();
+            double s2 = 0;
+            j = bis.size();
+            while (j > 0) {
+                BonusRule bo = bis.get(bis.size() - j);
+                if (b.profit() >= bo.minimum()) {
+                    s2 = bo.multiplier();
+                    j = 1;
                 }
                 j--;
             }
-            s += b.baseSalary() +(b.profit()*s2);
+            s += b.baseSalary() + (b.profit() * s2);
             i--;
         }
-        r= t -s;
-        return new CompanyProfit(t,s,r);
+        r = t - s;
+        return new CompanyProfit(t, s, r);
     }
 }
